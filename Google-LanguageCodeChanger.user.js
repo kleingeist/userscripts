326a70d5dfa3a4e5b0ed8710dd9411c4b2645c9a
kleingeist <kleingeist+git@googlemail.com>
2010-05-12 15:17:48 +0200

    Adjusted to new Google Layout / Ajax and introduced basal config prompt

--- Google-LanguageCodeChanger.user.js
+++ Google-LanguageCodeChanger.user.js
@@ -2,58 +2,117 @@
 // @name           Google Language Code Changer
 // @namespace      http://brainroom.ae35.de
 // @description    Add a select-box for the hl parameter on google result pages.
-// @version        1.0
+// @version        1.5
 // @copyright      2010+, kleingeist (http://github.com/kleingeist/userscripts)
 // @licence        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        http://www.google.*/search?*
+// @include        http://www.google*/#*q=*
+// @include        http://www.google.*/
+// @include        http://www.google.*/images?*
 // ==/UserScript==
 
+// load options
+var list = GM_getValue("selectedLangs", "").split(" ");
+
+// load the script dependend on ajax or normal google version
+if (window.location.hash.match(/q\=/) || window.location.pathname == '/') {
+	document.addEventListener('DOMAttrModified', DOMAttrModifiedListener, true);
+}
+else {
+	glcc(list);
+}
+
+function DOMAttrModifiedListener(e) {
+	// thanks to http://www.amirharel.com/2009/07/19/manipulating-google-results-ajax-version/ for the #foot hint
+	if (e.target.id == 'foot' && !document.getElementById('glccHl')) {
+		glcc(list);
+	}
+}
+
+GM_registerMenuCommand('Select Language Codes', function() { 
+	GM_setValue('selectedLangs'
+		, window.prompt('Languages shown in the selection (ex. de for german see http://sites.google.com/site/tomihasa/google-language-codes for full list)'
+			  , GM_getValue('selectedLangs', "")
+		)
+	);
+}, '', 's');
 
 
-(function glcc() {
-    // language list, borrowed from http://gpsgfaq.googlepages.com/google_language_codes.html
-    // You can change following list few for your main using. e.g., var list = ['en', 'ja', 'xx-hacker']
-    var list = 
-    ['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bh', 'xx-bork', 'bs', 'br', 'bg', 'km', 'ca',
-     'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'nl', 'xx-elmer', 'en', 'eo', 'et', 'fo', 'tl', 'fi',
-     'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gn', 'gu', 'xx-hacker', 'iw', 'hi', 'hu', 'is', 'id', 'ia', 'ga',
-     'it', 'ja', 'jw', 'kn', 'kk', 'xx-klingon', 'ko', 'ku', 'ky', 'lo', 'la', 'lv', 'ln', 'lt', 'mk',
-     'ms', 'ml', 'mt', 'mr', 'mo', 'mn', 'ne', 'no', 'nn', 'oc', 'or', 'ps', 'fa', 'xx-piglatin', 'pl',
-     'pt-BR', 'pt-PT', 'pa', 'qu', 'ro', 'rm', 'ru', 'gd', 'sr', 'sh', 'st', 'sn', 'sd', 'si', 'sk', 'sl',
-     'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'tt', 'te', 'th', 'ti', 'to', 'tr', 'tk', 'tw', 'ug', 'uk', 'ur',
-     'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu'];
+function glcc(list) {	
+
+	// Thanks to http://sites.google.com/site/tomihasa/google-language-codes for the list
+	var codes = 
+	['af', 'sq', 'am', 'ar', 'hy', 'az', 'eu', 'be', 'bn', 'bh', 'xx-bork', 'bs',
+	'br', 'bg', 'km', 'ca', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'nl', 
+	'xx-elmer', 'en', 'eo', 'et', 'fo', 'tl', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 
+	'el', 'gn', 'gu', 'xx-hacker', 'ha', 'iw', 'hi', 'hu', 'is', 'id', 'ia', 'ga', 
+	'it', 'ja', 'jw', 'kn', 'kk', 'rw', 'rn', 'xx-klingon', 'ko', 'ku', 'ky', 
+	'lo', 'la', 'lv', 'ln', 'lt', 'mk', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mo', 
+	'mn', 'sr-ME', 'ne', 'no', 'nn', 'oc', 'or', 'om', 'ps', 'fa', 'xx-pirate', 
+	'pl', 'pt-BR', 'pt-PT', 'pa', 'qu', 'ro', 'rm', 'ru', 'gd', 'sr', 'sh', 'st', 
+	'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tg', 'ta', 'tt', 
+	'te', 'th', 'ti', 'to', 'tr', 'tk', 'tw', 'ug', 'uk', 'ur', 'uz', 'vi', 'cy', 
+	'xh', 'yi', 'yo', 'zu'];
 	
-		
-	// get hl inputs
-	var hl = document.getElementsByName('hl');
+	var names =
+	['Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Azerbaijani', 
+	'Basque', 'Belarusian', 'Bengali', 'Bihari', 'Bork, bork, bork!', 'Bosnian', 
+	'Breton', 'Bulgarian', 'Cambodian', 'Catalan', 'Chinese (Simplified)', 
+	'Chinese (Traditional)', 'Corsican', 'Croatian', 'Czech', 'Danish', 'Dutch', 
+	'Elmer Fudd', 'English', 'Esperanto', 'Estonian', 'Faroese', 'Filipino', 
+	'Finnish', 'French', 'Frisian', 'Galician', 'Georgian', 'German', 'Greek', 
+	'Guarani', 'Gujarati', 'Hacker', 'Hausa', 'Hebrew', 'Hindi', 'Hungarian', 
+	'Icelandic', 'Indonesian', 'Interlingua', 'Irish', 'Italian', 'Japanese', 
+	'Javanese', 'Kannada', 'Kazakh', 'Kinyarwanda', 'Kirundi', 'Klingon', 'Korean', 
+	'Kurdish', 'Kyrgyz', 'Laothian', 'Latin', 'Latvian', 'Lingala', 'Lithuanian', 
+	'Macedonian', 'Malagasy', 'Malay', 'Malayalam', 'Maltese', 'Maori', 'Marathi', 
+	'Moldavian', 'Mongolian', 'Montenegrin', 'Nepali', 'Norwegian', 
+	'Norwegian (Nynorsk)', 'Occitan', 'Oriya', 'Oromo', 'Pashto', 'Persian', 
+	'Pirate', 'Polish', 'Portuguese (Brazil)', 'Portuguese (Portugal)', 'Punjabi', 
+	'Quechua', 'Romanian', 'Romansh', 'Russian', 'Scots Gaelic', 'Serbian', 
+	'Serbo-Croatian', 'Sesotho', 'Shona', 'Sindhi', 'Sinhalese', 'Slovak', 
+	'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili', 'Swedish', 'Tajik', 
+	'Tamil', 'Tatar', 'Telugu', 'Thai', 'Tigrinya', 'Tonga', 'Turkish', 'Turkmen', 
+	'Twi', 'Uighur', 'Ukrainian', 'Urdu', 'Uzbek', 'Vietnamese', 'Welsh', 'Xhosa', 
+	'Yiddish', 'Yoruba', 'Zulu']
 
+	if (!list || !list.length || list.length == 0 || (list.length == 1 && list[0] == "")) {
+		list = codes;
+	}
+
+	var hl = document.getElementsByName('hl');
+	
 	var current;
-	if (typeof(unsafeWindow.google) != 'undefined' && unsafeWindow.google.kHL)
-		current = unsafeWindow.google.kHL;
-	else if(hl.length > 0)
+	if(hl.length > 0)
 		current = hl[0].value;
 	else
-		current = myhl
+		current = list[0]
 
-	if (hl.length > 0)
-		hl[0].parentNode.removeChild(hl[0]);
+	if (hl.length > 0) {
+		for (i = hl.length - 1; i > -1; i--) {
+			hl[i].parentNode.removeChild(hl[i]);
+		}
+	}
 	
 	
-	function generateOption(v) {
-		return '<option value="' + v + '"' + ((v == current)? ' selected="1"' : '') + '>' + v + '</option>';
+	function generateOption(k) {
+		var index = codes.indexOf(k);
+		return '<option value="' + k + '"' + ((k == current)? ' selected="selected"' : '') + '>' + ((index == -1) ? k : names[index]) + '</option>';
 	};
 	
-	var sel = document.createElement('select');
-	sel.setAttribute('id','glccm');
-	sel.setAttribute('name', 'hl');
-	sel.setAttribute('STYLE', 'margin:2px 0 0 .4em;');
-	sel.innerHTML = list.map(generateOption).join("\n");		
-	document.getElementsByName('btnG')[0].parentNode.appendChild(sel);
-
+	var container = document.createElement('span');
+	container.style.lineHeight = "30px";
 	
-	var parentForm = sel.parentNode;
-	while(parentForm.nodeName.toUpperCase() != 'FORM') {
-		parentForm = parentForm.parentNode;
-	}
-	sel.addEventListener("change", function (event) { parentForm.submit(); }, true);
-})();
+	var sel = document.createElement('select');
+	sel.setAttribute('id','glccHl');
+	sel.setAttribute('name', 'hl');
+	sel.style.margin = "0 2px";
+	sel.innerHTML = list.map(generateOption).join("\n");
+	container.appendChild(sel);
+
+	var button = document.getElementsByName('btnG')[0];
+	button.parentNode.style.whiteSpace = "nowrap";
+	button.parentNode.appendChild(container);
+	
+	sel.addEventListener("change", function (event) { document.getElementsByName('btnG')[0].click(); }, true);
+};
